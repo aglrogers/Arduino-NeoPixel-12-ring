bf6775d57feb25ac15ca0a879434e6ae06de834f
Andrew Rogers <104739481+aglrogers@users.noreply.github.com>
2022-09-05 03:17:20 +0100

    Added button control
    The code now has a feature that allows the user to press a button, changing the rotation from clockwise to anticlockwise and vice-versa with each press.

--- 12-ring-rotate-loop/12-ring-rotate-loop.ino
+++ 12-ring-rotate-loop/12-ring-rotate-loop.ino
@@ -6,10 +6,7 @@
 
 #include <Adafruit_NeoPixel.h> // Official NeoPixel library
 
-// Direction of rotation
-// 0 = clockwise
-// 1 = anticlockwise
-const int rotation = 0;
+const int button_pin = 3;
 
 // The board pin for the NeoPixel ring
 const int neopixel_pin = 2;
@@ -22,21 +19,38 @@ const int pixel_amount = 12;
 const int pixel_brightness = 20;
 
 // Delay between changes in milliseconds
-const int delay_interval = 200;
+const int delay_interval = 100;
 
 // Iterator variables
 int current_pixel = 0;  // Current value
 int ring_iterator = 0;  // Next value
 
+// Direction of rotation
+// 0 = clockwise
+// 1 = anticlockwise
+int rotation = 0;
+
+// Button detection variables
+int reading = 0;
+int last_state = 0;
+int button_down = 0;
+
+// Amount of time button is held before something happens
+int debounce_buffer = 60;
+
 // Variables for pause with using Delay()
 unsigned long current_ms = 0;
 unsigned long previous_ms = 0;
+unsigned long debounce_ms = 0;
 
 // Initialising the NeoPixel ring with the number of pixels and the board pin
 Adafruit_NeoPixel ring(pixel_amount, neopixel_pin, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
+  // Set up button pin as input
+  pinMode(button_pin, INPUT);
+
   // Start the ring
   ring.begin();
 
@@ -52,6 +66,35 @@ void loop()
 {
   // Update millisecond counter
   current_ms = millis();
+
+  // Detect button activity
+  reading = digitalRead(button_pin);
+
+  // If the button state has changed
+  if (reading != last_state)
+  {
+    // Take a record of the current amount of milliseconds elapsed
+    debounce_ms = current_ms;
+  }
+
+  // If the button has been held down for longer than the debounce buffer value
+  if (current_ms - debounce_ms >= debounce_buffer && reading && !button_down)
+  {
+    // Record the button as being pressed
+    button_down = 1;
+
+    // Reverse the rotation of the ring pattern
+    rotation = !rotation;
+  }
+
+  // Record the button being released
+  if (reading == LOW)
+  {
+    button_down = 0;
+  }
+
+  // Record the state of the button
+  last_state = reading;
 
   // If a delay interval has elapsed
   if (current_ms - previous_ms >= delay_interval)
